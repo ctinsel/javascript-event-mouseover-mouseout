94f19168d8c57a2da73eb16bbaab5c7aa54e6bf9
Nosa Ayere <nosaayere@gmail.com>
2018-07-31 20:06:06 +0200

    mouseover javasript program

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+document.querySelector('.grid').addEventListener('mouseover', function(e) {
+  if (e.target.tagName === 'IMG') {
+
+    var myElement = document.createElement('div');
+    myElement.id = "roller";
+    myElement.className = 'preview';
+    e.target.parentNode.appendChild(myElement);
+
+    var myImg = document.createElement('img');
+    var imgLoc = e.target.src;
+    myImg.src = imgLoc.substr(0, imgLoc.length-7) + '.jpg';
+
+    myElement.appendChild(myImg);
+
+    e.target.addEventListener('mouseout', function handler(h) {
+      var myNode = h.target.parentNode.querySelector('div.preview');
+      myNode.parentNode.removeChild(myNode);
+      e.target.removeEventListener('mouseout', handler, false)
+    }, false);
+
+  } // check to see that I clicked on IMG only
+}, false); // click event
